4f1c057242d65cc56bac62b770ee7238667ed414
Shane <mhurtado1@berklee.edu>
2023-01-19 14:25:13 -0600

    more honey rae

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,12 +1,28 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import "./Tickets.css"
 
 export const TicketList = () => {
     const [tickets, setTickets] = useState([])
     const [filteredTickets, setFiltered] = useState([])
+    const [emergency, setEmergency] = useState(false)
+    const navigate = useNavigate()
 
     const localHoneyUser = localStorage.getItem("honey_user")
     const honeyUserObject = JSON.parse(localHoneyUser)
+
+    useEffect(
+        () => {
+            if (emergency) {
+                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
+                setFiltered(emergencyTickets)
+            }
+            else {
+                setFiltered(tickets)
+            }
+        },
+        [emergency] //observing emergency 
+    )
 
     useEffect(
         () => {
@@ -34,10 +50,19 @@ export const TicketList = () => {
     }, [tickets])
 
     return <>
+        {
+            honeyUserObject.staff //tenary statement
+                ? <>
+                 <button onClick={() => { setEmergency(true) }}>Emergency Only</button>
+                 <button onClick={() => { setEmergency(false) }}>Show All</button>
+                 </>
+                : <button onClick={() => navigate("/ticket/create")}>Create Ticket</button>
+        }
+
         <h2>List of Tickets</h2>
         <article className="tickets">
             {
-                tickets.map((ticket) => {
+                filteredTickets.map((ticket) => {
                     return <section className="ticket">
                         <header>{ticket.description}</header>
                         <footer>Emergency: {ticket.emergency ? "Yes" : "No"}</footer>
