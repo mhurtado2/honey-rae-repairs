f2585ca8b388b69f037d0b45884c111b3396ec9f
Shane <mhurtado1@berklee.edu>
2023-01-24 08:24:10 -0600

    some changes

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,25 +1,21 @@
-import { Link, useNavigate } from "react-router-dom"
+
+import { CustomerNav } from "./CustomerNav"
+import { EmployeeNav } from "./EmployeeNav"
 import "./NavBar.css"
 
-export const NavBar = () => {
-    const navigate = useNavigate()
 
-    return (
-        <ul className="navbar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/tickets">Tickets</Link>
-            </li>
-            {
-                localStorage.getItem("honey_user")
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("honey_user")
-                            navigate("/", {replace: true})
-                        }}>Logout</Link>
-                    </li>
-                    : ""
-            }
-        </ul>
-    )
+export const NavBar = () => {
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+    if (honeyUserObject.staff) {
+        //return employy views
+        return <EmployeeNav />
+    }
+    else {
+        return <CustomerNav />
+    }
+
 }
 
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,22 +1,17 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { TicketForm } from "../tickets/TicketForm"
-import { TicketContainer } from "../tickets/TicketContainer"
+import { CustomerViews } from "./CustomerViews"
+import { EmployeeViews } from "./EmployeeViews"
 
 export const ApplicationViews = () => {
-    return (
-        <Routes>
-            <Route path="/" element={
-                <>
-                    <h1>Honey Rae Repair Shop</h1>
-                    <div>Your one-stop-shop to get all your electronics fixed</div>
 
-                    <Outlet />
-                </>
-            }>
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
 
-                <Route path="tickets" element={ <TicketContainer/> } />
-                <Route path="ticket/create" element={<TicketForm />} />
-            </Route>
-        </Routes>
-    )
+    if (honeyUserObject.staff) {
+        //return employy views
+        return <EmployeeViews />
+    }
+    else {
+        return <CustomerViews />
+    }
+
 }
